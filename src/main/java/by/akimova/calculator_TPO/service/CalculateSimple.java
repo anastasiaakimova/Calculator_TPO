819411263c66e2b5678b83fe30ab7a487f635d96
Anastasia Sitsko <anastasia.akimova.v@gmail.com>
2025-10-19 19:33:50 +0300

    Create github actions

--- src/main/java/by/akimova/calculator_TPO/service/CalculateSimple.java
+++ src/main/java/by/akimova/calculator_TPO/service/CalculateSimple.java
@@ -23,31 +23,4 @@ public class CalculateSimple {
         model.setB(0);
         return model;
     }
-//    public int add(OperationModel model) {
-//        return model.getA() + model.getB();
-//    }
-//
-//    public int subtract(OperationModel model) {
-//        return model.getA() - model.getB();
-//    }
-//
-//    public int multiply(OperationModel model) {
-//        return model.getA() * model.getB();
-//    }
-//
-//    public double divide(OperationModel model) {
-//        if (model.getA() == 0) return 0;
-//        if (model.getB() == 0) return 0;
-//        return (double) model.getA() / model.getB();
-//    }
-//
-//    public double sqrt(OperationModel model) {
-//        return Math.sqrt(model.getB());
-//    }
-//
-//    public int power(OperationModel model) {
-//        return model.getB() * model.getB();
-//    }
-
-
 }
